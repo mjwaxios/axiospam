463c235d091f8af5f7105597f4b01f5acb9a456d
mwyrick <mjw@axiosengineering.com>
2020-07-15 14:18:41 -0400

    Added examples for the Methods

--- pamuser_test.go
+++ pamuser_test.go
@@ -17,3 +17,29 @@ func Example() {
 	// Person testana Authenticated: false, Reason: Authenticate not run yet
 	// Person testana Authenticated: true, Reason: <nil>
 }
+
+func ExamplePAMUser() {
+	p := axiospam.PAMUser{Username: "testana", Password: "thisisatest123"}
+	if p.Username == "testana" && p.Password == "thisisatest123" {
+		fmt.Print("User is Ok")
+	}
+	// Output:
+	// User is Ok
+}
+
+func ExamplePAMUser_Authenticate() {
+	p := axiospam.PAMUser{Username: "testana", Password: "thisisatest123"}
+	auth, reason := p.Authenticate()
+	fmt.Printf("Person %s Authenticated: %v, Reason: %v\n", p.Username, auth, reason)
+	// Output:
+	// Person testana Authenticated: true, Reason: <nil>
+}
+
+func ExamplePAMUser_IsAuthenticated() {
+	p := axiospam.PAMUser{Username: "testana", Password: "thisisatest123"}
+	p.Authenticate()
+	auth, reason := p.IsAuthenticated()
+	fmt.Printf("Person %s Authenticated: %v, Reason: %v\n", p.Username, auth, reason)
+	// Output:
+	// Person testana Authenticated: true, Reason: <nil>
+}
