3682618ad75668356960b51610dd5a2593820331
Michael Wyrick <Michael.Wyrick@caci.com>
2020-11-18 13:19:53 -0500

    Really Cleaned up the interfaces

--- pamuser.go
+++ pamuser.go
@@ -27,12 +27,12 @@
 //  $cat /etc/pam.d/axiospam
 //  #%PAM-1.0
 //  # default for a centos system,
-//  auth	   required	pam_sepermit.so
+//  auth	   required	Pamsepermit.so
 //  auth       substack     password-auth
 //  auth       include      postlogin
 //
 //  # if we only want the local system use only this
-//  #auth	required	pam_unix.so
+//  #auth	required	Pamunix.so
 //
 // This also links to libpam so you will need to have libpam-devel installed.
 // on Ubuntu the pam-devel package is called libpam0g-dev
@@ -46,18 +46,50 @@ import (
 	"sync"
 )
 
+var (
+	errUnknownFlag = errors.New("unknown flag on account")
+)
+
+// PamResult is the result of a call to Authenticate
+type PamResult int
+
 const (
-	success         = 0
-	authError       = 7
-	authinfoUnavail = 9
-	userUnknown     = 10
-	newAuthtokReqd  = 12
-	acctExpired     = 13
-	authtokErr      = 20
+	PamSuccess             PamResult = 0  /* Successful function return */
+	PamOpenERR             PamResult = 1  /* dlopen() failure when dynamically */
+	PamSymbolERR           PamResult = 2  /* Symbol not found */
+	PamServiceERR          PamResult = 3  /* Error in service module */
+	PamSystemERR           PamResult = 4  /* System error */
+	PamBufERR              PamResult = 5  /* Memory buffer error */
+	PamPermDenied          PamResult = 6  /* Permission denied */
+	PamAuthERR             PamResult = 7  /* Authentication failure */
+	PamCredInsufficient    PamResult = 8  /* Can not access authentication data */
+	PamAuthInfoUnavail     PamResult = 9  /* Underlying authentication service */
+	PamUserUnknown         PamResult = 10 /* User not known to the underlying */
+	PamMaxTries            PamResult = 11 /* An authentication service has */
+	PamNewAuthTokReqd      PamResult = 12 /* New authentication token required. */
+	PamAcctExpired         PamResult = 13 /* User account has expired */
+	PamSessiionERR         PamResult = 14 /* Can not make/remove an entry for */
+	PamCredUnavail         PamResult = 15 /* Underlying authentication service */
+	PamCredExpired         PamResult = 16 /* User credentials expired */
+	PamCredERR             PamResult = 17 /* Failure setting user credentials */
+	PamNoModuleData        PamResult = 18 /* No module specific data is present */
+	PamConvERR             PamResult = 19 /* Conversation error */
+	PamAuthTokERR          PamResult = 20 /* Authentication token manipulation error */
+	PamAuthTokRecoveryERR  PamResult = 21 /* Authentication information */
+	PamAuthTokLockBusy     PamResult = 22 /* Authentication token lock busy */
+	PamAuthTokDisableAging PamResult = 23 /* Authentication token aging disabled */
+	PamTryAgain            PamResult = 24 /* Preliminary check by password service */
+	PamIgnore              PamResult = 25 /* Ignore underlying account module */
+	PamAbort               PamResult = 26 /* Critical error (?module fail now request) */
+	PamAuthTokExpired      PamResult = 27 /* user's authentication token has expired */
+	PamModuleUnknown       PamResult = 28 /* module is not known */
+	PamBadItem             PamResult = 29 /* Bad item passed to Pam*_item() */
+	PamConvAgain           PamResult = 30 /* conversation function is event driven and data is not available yet */
+	PamIncomplete          PamResult = 31 /* please call this function again to */
 )
 
 // messages Number to Strings
-var messages = []string{
+var messages = [...]string{
 	"SUCCESS",
 	"OPEN_ERR",
 	"SYMBOL_ERR",
@@ -92,50 +124,22 @@ var messages = []string{
 	"INCOMPLETE",
 }
 
-// PamResult is the result of a call to Authenticate
-type PamResult int
-
-const (
-	// Uninitialized is the zero value
-	Uninitialized PamResult = iota
-	// SystemError can happen if pam is not setup correctly or the application does not have permission for the user in question
-	SystemError
-	// Authenticated Username and Password are Valid and no flags on the account
-	Authenticated
-	// UnknownUser means the Username is Unknown to the pam system
-	UnknownUser
-	// AuthError can happen if the password is incorrect, the account is locked, etc.
-	AuthError
-	// PasswordExpired means Username and Password are Valid but the system indicates the Password has expired
-	PasswordExpired
-	// AccountExpired means Username and Password are Valid but the system indicates the Account has expired
-	AccountExpired
-	// UnknownFlag is a catchall for a pam account flag we don't know about
-	UnknownFlag
-	// AuthTokError means the New password does not meet requirements or there was a problem updating the Auth Token
-	AuthTokError
-	// InvalidoldPassword means the Old password is invalid
-	InvalidoldPassword
-	// Success means the password was changed
-	Success
-)
-
+// String will convert a PamResult to a String
 func (s PamResult) String() string {
-	if s < Uninitialized || s > Success {
+	if int(s) < 0 || int(s) > len(messages) {
 		return "unknown AuthenResult"
 	}
 
-	return [...]string{"uninitialized", "system error", "authenticated", "unknown user",
-		"auth error", "password expired", "account expired", "unknown account flag",
-		"unable to update auth token", "invalid old password", "success"}[s]
+	return messages[s]
 }
 
+// Error will let use use a PamResult as an Error, will use string to make it a String
 func (s PamResult) Error() string {
 	return "pam error: " + s.String()
 }
 
 // AccountFlags get the User Account Flags from Pam
-func AccountFlags(name string) (int, error) {
+func AccountFlags(name string) (PamResult, error) {
 	flags, err := getUserAccountFlags(name, true)
 	return flags, err
 }
@@ -145,89 +149,73 @@ func Authenticate(name, password string) (PamResult, error) {
 	// Check that we can get the Account Info for this user,
 	// we will also check the flags again after we authenticate
 	Flags, _ := getUserAccountFlags(name, true)
+
 	switch Flags {
-	case success:
+	case PamSuccess, PamNewAuthTokReqd, PamAuthTokExpired:
 		break
-	case userUnknown:
-		return UnknownUser, UnknownUser
-	case newAuthtokReqd:
-		break
-	case acctExpired:
-		return AccountExpired, AccountExpired
+	case PamAuthInfoUnavail, PamUserUnknown, PamAcctExpired:
+		return PamAuthERR, Flags
 	default:
-		return SystemError, UnknownFlag
+		return PamSystemERR, errUnknownFlag
 	}
 
 	a, err := isUserLoginToken(name, password, false)
 	if err != nil {
-		return SystemError, err
+		return PamSystemERR, err
 	}
 
 	// Did not Authenticate and did not have an error
-	if a == false {
-		return AuthError, AuthError
+	// We return an AuthERR and the result from the pam call might have more information
+	if a != PamSuccess {
+		return PamAuthERR, a
 	}
 
 	// We are Authenticated from this point on
 
 	// We Are Valid, so check if we should return any flags for the account
 	Flags, _ = getUserAccountFlags(name, true)
+
 	switch Flags {
-	case success:
-		return Authenticated, nil
-	case newAuthtokReqd:
-		return PasswordExpired, nil
-	case acctExpired:
-		return AccountExpired, nil
-	default:
-		return SystemError, UnknownFlag
+	case PamSuccess, PamNewAuthTokReqd, PamAcctExpired:
+		return Flags, nil
 	}
+
+	return PamSystemERR, errUnknownFlag
 }
 
 // ChangePassword will call the pam system to change the users password
 func ChangePassword(name, oldPassword, newPassword string) (PamResult, error) {
 	// Check that we can get the Account Info for this user,
 	Flags, _ := getUserAccountFlags(name, true)
+
 	switch Flags {
-	case success:
+	case PamSuccess, PamNewAuthTokReqd, PamAcctExpired:
 		break
-	case userUnknown:
-		return UnknownUser, UnknownUser
-	case newAuthtokReqd:
-		break
-	case acctExpired:
-		break
+	case PamUserUnknown, PamAuthInfoUnavail:
+		return PamAuthERR, Flags
 	default:
-		return SystemError, UnknownFlag
+		return PamSystemERR, errUnknownFlag
 	}
 
 	// Continue to Change Password
 	status, err := changeToken(name, oldPassword, newPassword, false)
-
 	if err != nil {
-		return SystemError, err
+		return PamSystemERR, err
 	}
 
 	switch status {
-	case success:
-		return Success, nil
-	case authError:
-		return InvalidoldPassword, InvalidoldPassword
-	case authtokErr:
-		return AuthTokError, AuthTokError
-	default:
-		return SystemError, errors.New("unknown status")
+	case PamSuccess:
+		return PamSuccess, nil
+	case PamAuthERR, PamAuthTokERR:
+		return status, status
 	}
+
+	return PamSystemERR, errUnknownFlag
 }
 
 // ------------------------------------------------------------------------------------
 // Private Functtions to call the pam C interface
 // ------------------------------------------------------------------------------------
-
-// Pam error values
-var (
-	errPassphrase = errors.New("incorrect login")
-)
 
 // Global state is needed for the PAM callback, so we guard this function with a
 // lock. tokenToCheck is only ever non-nil when tokenLock is held.
@@ -269,7 +257,7 @@ func passphraseInput(prompt *C.char) *C.char {
 // IsUserLoginToken returns nil if the presented token is the user's login key,
 // and returns an error otherwise. Note that unless we are currently running as
 // root, this check will only work for the user running this process.
-func isUserLoginToken(username string, password string, quiet bool) (bool, error) {
+func isUserLoginToken(username string, password string, quiet bool) (PamResult, error) {
 	// We require global state for the function. This function never takes
 	// ownership of the token, so it is not responsible for wiping it.
 	tokenLock.Lock()
@@ -283,24 +271,25 @@ func isUserLoginToken(username string, password string, quiet bool) (bool, error
 
 	transaction, err := start("axiospam", username)
 	if err != nil {
-		return false, err
+		return PamSystemERR, err
 	}
 	defer transaction.End()
 
 	// Ask PAM to authenticate the token.
 	authenticated, err := transaction.authenticate(quiet)
 	if err != nil {
-		return false, err
+		return PamSystemERR, err
 	}
 
-	if !authenticated {
-		return false, nil
+	if authenticated {
+		return PamSuccess, nil
 	}
-	return true, nil
+
+	return PamAuthERR, nil
 }
 
 // changeToken will change the users password
-func changeToken(username, oldpassword, newpassword string, quiet bool) (int, error) {
+func changeToken(username, oldpassword, newpassword string, quiet bool) (PamResult, error) {
 	// We require global state for the function. This function never takes
 	// ownership of the token, so it is not responsible for wiping it.
 	tokenLock.Lock()
@@ -314,18 +303,15 @@ func changeToken(username, oldpassword, newpassword string, quiet bool) (int, er
 
 	transaction, err := start("axiospam", username)
 	if err != nil {
-		return 5, err
+		return PamSystemERR, err
 	}
 	defer transaction.End()
 
 	// Ask PAM to authenticate the old Token First
-	authenticated, err := transaction.authenticate(quiet)
-	if err != nil {
-		return -1, err
-	}
-
-	if !authenticated {
-		return authError, nil
+	if authenticated, err := transaction.authenticate(quiet); err != nil {
+		return PamSystemERR, err
+	} else if !authenticated {
+		return PamAuthERR, nil
 	}
 
 	tokenToCheck = oldpassword
@@ -333,22 +319,22 @@ func changeToken(username, oldpassword, newpassword string, quiet bool) (int, er
 
 	// Ask PAM to change the token.
 	status, err := transaction.changeTok(quiet)
-	return status, err
+	return PamResult(status), err
 }
 
 // get the User Account Flags from PAM
-func getUserAccountFlags(username string, quiet bool) (int, error) {
+func getUserAccountFlags(username string, quiet bool) (PamResult, error) {
 	transaction, err := start("axiospam", username)
 	if err != nil {
-		return 0, err
+		return PamSystemERR, err
 	}
 	defer transaction.End()
 
 	// Ask PAM to authenticate the token.
 	flags, err := transaction.accountManagement(quiet)
 	if err != nil {
-		return 0, err
+		return PamSystemERR, err
 	}
 
-	return flags, nil
+	return PamResult(flags), nil
 }
